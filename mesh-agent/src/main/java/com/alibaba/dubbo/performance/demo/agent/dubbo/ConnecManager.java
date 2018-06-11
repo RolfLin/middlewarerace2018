7c2a38b651cfc6fcfcde06321754ec51cd047e40
15622363122 <455035961@qq.com>
2018-06-12 00:10:30 +0800

    0612_2

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/dubbo/ConnecManager.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/dubbo/ConnecManager.java
@@ -35,8 +35,8 @@ public class ConnecManager {
         if (null == channel) {
             synchronized (lock){
                 if (null == channel){
-                    int port = Integer.valueOf(System.getProperty("dubbo.protocol.port"));
-                    channel = bootstrap.connect("127.0.0.1", port).sync().channel();
+//                    int port = Integer.valueOf(System.getProperty("dubbo.protocol.port"));
+                    channel = bootstrap.connect("127.0.0.1", 20880).sync().channel();
                 }
             }
         }
